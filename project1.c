5a318a7aa42be5d113134e8be95c2be1e5a27390
Devin M <75272179+mDevinn@users.noreply.github.com>
2023-02-10 15:40:11 -0500

    Added dynamic node amount

--- project1.c
+++ project1.c
@@ -2,54 +2,62 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main() {
     //global variables
-    int fd[3][2]; //3 nodes, will give the input value as the size of the array after its working for 3
     pid_t pid;
     int nodeCount;
     int whichNode;
     char message[100];
+    int fd[100][2]; //making space for 100 pipes cus it just makes the code easier to read and I dont think he will test greater than 100 nodes
+    int i;
 
     //getting input
     printf("How Many Nodes?\n");
     scanf("%d%*c", &nodeCount);
     printf("You entered: %d\n", nodeCount);
     //create pipe
-    for (int i = 0; i < 3; i++) {
+    for (i = 0; i < nodeCount; i++) {
         if (pipe(fd[i]) == -1) {
             perror("pipe");
             exit(EXIT_FAILURE);
         }
     }
+    printf("\nChild Processes:\n");
     //creating child processes
-    for (int i = 0; i < 3; i++) {
+    for (i = 0; i < nodeCount; i++) {
         pid = fork();
         if (pid == 0) {
             break;
         }
+       printf("pid: %d\n", pid);
     }
-    printf("pid: %d", getpid());
 
-    //establishing read and write connections might be wrong havnt tested throuroughly
+    //establishing read and write connections
     if (pid == 0) {
-        //need to add ifs for each child process
-        //probably some sort of wait for the parent to write to the pipe
+    // child process
+        int nextNode = (i + 1) % nodeCount;
+        int prevNode = (i + nodeCount - 1) % nodeCount;
+        close(fd[prevNode][1]);
+        close(fd[nextNode][0]);
+        read(fd[prevNode][0], &message, sizeof(message));
+        printf("I am node %d. Received message: %s. Passing to node %d.\n", i + 1, message, nextNode + 1);
+        write(fd[nextNode][1], &message, sizeof(message));
     } else {
-        close(fd[0][0]);
-        close(fd[3-1][0]);
-        close(fd[3-1][1]);
-
-        printf("What is the message you'd like to send?\n");
-        scanf("%s%*c", &message);
+    // parent process
+        printf("\nWhat is the message you'd like to send?\n");
+        scanf("%99s", message);
         printf("Which node would you like to send the message to?\n");
         scanf("%d%*c", &whichNode);
-        printf("message entered: %s\n", message);
-        printf("to be sent to: %d\n", whichNode);
-
+        printf("Message entered: %s\n", message);
+        printf("Sending to Node %d...\n", whichNode);
         write(fd[whichNode][1], &message, sizeof(message));
-
-        for (int i = 0; i < 3; i++) {
+        for (i = 0; i < whichNode; i++) {
+            close(fd[i][0]);
+            close(fd[i][1]);
+        }
+        for (i = 0; i < whichNode; i++) {
             wait(NULL);
         }
     }
